2c69b51f594a4d38cdcc7d57c48f09d2fd97aefb
Tom Darneix <tomdar87@outlook.com>
2021-06-01 17:22:42 +0200

    core(npm): Re-arrange package JSON files (#2)
    * Clean up entry package JSON
    
    * Use the local eslint configs
    
    * Fix typo
    
    * Attempt to dependencies
    
    * Re-arrange dependencies to expose the centreon UI deps
    
    * Add code owners
    
    * Move local deps to devDependencies
    
    * Move back the local dependencies to 'dependencies'
    
    * Move frontend-config to dependencies
    
    * Move back local deps to devDependencies

--- packages/frontend-config/jest/centreon-ui.js
+++ packages/frontend-config/jest/centreon-ui.js
@@ -1,10 +1,10 @@
 module.exports = {
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-  },
   moduleNameMapper: {
     '\\.(s?css|png|svg)$': 'identity-obj-proxy',
   },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
 };
