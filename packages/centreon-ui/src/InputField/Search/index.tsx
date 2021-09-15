81f906d5e1b263645f64ab1d81f23fd57277b24d
Jordan CARO <jcaro@centreon.com>
2021-09-15 17:48:28 +0200

    Add EndAdornment to add cancelIcon in search field

--- packages/centreon-ui/src/InputField/Search/index.tsx
+++ packages/centreon-ui/src/InputField/Search/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import IconSearch from '@material-ui/icons/Search';
+import CancelIcon from '@material-ui/icons/Cancel';
 
 import TextField, { Props as TextFieldProps } from '../Text';
 
-type Props = Omit<TextFieldProps, 'StartAdornment'>;
+type Props = Omit<TextFieldProps, 'StartAdornment' | 'EndAdornment'>;
 
 const SearchField = (props: Props): JSX.Element => (
-  <TextField StartAdornment={(): JSX.Element => <IconSearch />} {...props} />
+  <TextField
+    EndAdornment={(): JSX.Element => <CancelIcon />}
+    StartAdornment={(): JSX.Element => <IconSearch />}
+    {...props}
+  />
 );
 
 export default SearchField;
