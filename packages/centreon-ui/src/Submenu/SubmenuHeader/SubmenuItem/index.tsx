e24684b43008b60e7a077af85443bbf329c0d40a
Jordan CARO <jcaro@centreon.com>
2022-03-02 10:00:17 +0100

    enh(UI): Add props to track user when they click on component #122

--- packages/centreon-ui/src/Submenu/SubmenuHeader/SubmenuItem/index.tsx
+++ packages/centreon-ui/src/Submenu/SubmenuHeader/SubmenuItem/index.tsx
@@ -63,15 +63,19 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 interface Props {
+  countTestId?: string;
   dotColored?: string;
   submenuCount: string | number;
   submenuTitle: string;
+  titleTestId?: string;
 }
 
 const SubmenuItem = ({
   dotColored,
   submenuTitle,
   submenuCount,
+  titleTestId,
+  countTestId,
 }: Props): JSX.Element => {
   const classes = useStyles();
 
@@ -81,13 +85,14 @@ const SubmenuItem = ({
         className={clsx(classes.title, {
           [classes.dotted]: !!dotColored,
         })}
+        data-testid={titleTestId}
       >
         <span className={clsx(classes.dot, classes[dotColored || ''])} />
         <Typography className={classes.titleContent} variant="body2">
           {submenuTitle}
         </Typography>
       </span>
-      <span className={classes.count}>
+      <span className={classes.count} data-testid={countTestId}>
         <Typography variant="body2">{submenuCount}</Typography>
       </span>
     </li>
